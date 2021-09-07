ec20d44b31a861984b042c76c960bbc3d5f448fd
MihanEntalpo <mihanentalpo@yandex.ru>
2021-09-07 11:02:43 +0700

    Изменены кнопки

--- web-app/templates/clipboard.php
+++ web-app/templates/clipboard.php
@@ -4,20 +4,39 @@
     <div class="col-8 messages-wrapper">
       Messages
       <div class='send'>
+          <!--
           <div class='buttons'>
-              <button type=button class='btn btn-sm btn-light' id='send_file_button' 
+              <button type=button class='btn btn-sm btn-primary' id='send_file_button' 
                       title='Send file' 
                       onclick='lib.ui.msg.send_file_btn_click()'
                       ><i class='fa fa-paperclip idle'></i><i class='fa fa-spin fa-cog loading'></i></button><br>
-              <button type=button class='btn btn-sm btn-light' id='send_text_button' 
+              <button type=button class='btn btn-sm btn-success' id='send_text_button' 
                       title='Send message (Ctrl+Enter)' 
                       onclick='lib.ui.msg.send_btn_click()'
                       onkeydown='lib.ui.msg.send_ctrl_enter()'
                       ><i class='fa fa-arrow-circle-right idle'></i><i class='fa fa-spin fa-cog loading'></i></button>
               <input type="file"  style="display: none;" id="file-selector" multiple onchange="lib.ui.msg.send_file_selected(event, this)">
           </div>
+          -->
           <div class='text'>
                 <textarea name='text' id='text_to_send'></textarea>
+          </div>
+          <div class="horizontal-buttons">
+              <button type=button class='btn btn-sm btn-light' id='send_file_button' 
+                      title='Send file' 
+                      onclick='lib.ui.msg.send_file_btn_click()'
+                      >
+                      Send file
+                      <i class='fa fa-paperclip idle'></i></button>
+              <button type=button class='btn btn-sm btn-light' id='send_text_button' 
+                      title='Send message (Ctrl+Enter)' 
+                      onclick='lib.ui.msg.send_btn_click()'
+                      onkeydown='lib.ui.msg.send_ctrl_enter()'
+                      disabled
+                      >
+                      Send message <span class="grey">(ctrl+enter)</span> <i class='fa fa-arrow-circle-right idle'></i>
+                        </button>
+              <input type="file"  style="display: none;" id="file-selector" multiple onchange="lib.ui.msg.send_file_selected(event, this)">
           </div>
       </div>
       <div class="messages">
