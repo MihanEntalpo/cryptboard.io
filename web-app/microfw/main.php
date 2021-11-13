4f1a5a6de35e9def4081418e54d7604d2f8c3c4f
MihanEntalpo <mihanentalpo@yandex.ru>
2021-11-14 01:45:07 +0700

    Added Sentry support

--- web-app/microfw/main.php
+++ web-app/microfw/main.php
@@ -5,4 +5,7 @@ require_once __DIR__ . "/route.php";
 require_once __DIR__ . "/storage.php";
 require_once __DIR__ . "/jwt.php";
 
-
+if (get_conf("SENTRY_DSN"))
+{
+    \Sentry\init(['dsn' => get_conf("SENTRY_DSN") ]);
+}
